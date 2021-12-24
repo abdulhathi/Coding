584020d37b83cbfc38a34eed4b54b0440bdc0f2e
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2021-12-23 23:33:33 -0800

    First time

---
+++ C#Basics/006.UserInput.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class UserInput {
+    public static void StringInterpolation() {
+        var name = Console.ReadLine();
+        Console.WriteLine($"Hello my name is {name}");
+    }
+}
---
+++ C#Basics/007.VariablesAndExpressions.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class VariablesAndExpressions {
+    // Variables    - store some values
+    // Identifier   - The name of the declared variable name Ex: int x = 5, Here x is the identifier
+    // Expression   - Evaluate to a value. 
+    // Operator     - It works with an operands Ex: +, -, &
+    // Operands     - The things can work with operator
+    public static VarsAndExpressions() {
+
+    }
+}
